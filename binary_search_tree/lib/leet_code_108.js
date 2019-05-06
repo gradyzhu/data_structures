88ae570b4093c7be7557ad43fb7b42fb207e7b1d
your name here <gradyzhoo@gmail.com>
2019-05-05 19:22:48 -0700

    Update BST

--- binary_search_tree/lib/leet_code_108.js
+++ binary_search_tree/lib/leet_code_108.js
@@ -2,6 +2,16 @@
 //  https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 
 
-function sortedArrayToBST(nums) {
-
-}
+var sortedArrayToBST = function(nums, root=null) {
+  if (!nums.length) return null;
+  let idx = Math.floor(nums.length / 2);
+  if (!root) root = new TreeNode(nums[idx]);
+  
+  let left = nums.slice(0, idx);
+  let right = nums.slice(idx + 1);
+  
+  root.left = sortedArrayToBST(left, left[nums.length -1]);
+  root.right = sortedArrayToBST(right, right[nums.length -1]);
+  
+  return root;
+}; 
--- binary_search_tree/lib/leet_code_110.js
+++ binary_search_tree/lib/leet_code_110.js
@@ -1,7 +1,16 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/balanced-binary-tree/
 
+const findHeight = root => {
+  if (!root) return -1;
+  let left = findHeight(root.left);
+  let right = findHeight(root.right);
+  let max = left > right ? left : right;
+  return 1 + max;
+};
 
-function isBalanced(root) {
-
-}
+var isBalanced = function(root) {
+  if (!root) return true;
+  let balanced = Math.abs(findHeight(root.left) - findHeight(root.right)) <= 1;
+  return balanced && isBalanced(root.left) && isBalanced(root.right);
+};
