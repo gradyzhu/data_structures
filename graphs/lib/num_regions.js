d0704303d471250f8fed1a173f82c808df7df9a3
your name here <gradyzhoo@gmail.com>
2019-05-12 23:43:28 -0700

    Add Graphs pt. 2

--- graphs/lib/num_regions.js
+++ graphs/lib/num_regions.js
@@ -1,18 +1,51 @@
-function numRegions(graph) {
-  let nums = 0;
-  let keys = Object.keys(graph);
-  if (!keys.length) return 0;
-  let visited = new Set();
-  // console.log(queue);
+// function numRegions(graph) {
+//   let regions = 0;
+//   let visited = new Set();
 
-  while (keys.length) {
-    let key = queue.shift();
-    if (visited.has(key)) continue;
-    visited.add(key);
-    nums++;
-    queue.push(...graph[key]);
+//   for (let key in graph) {
+//     if (visited.has(key)) continue;
+
+//     visited.add(key);
+
+//     let allVisited = true;
+//     let neighbors = graph[key];
+
+//     if (!neighbors.length) {
+//       regions++;
+//       continue;
+//     }
+    
+//     neighbors.forEach(neighbor => {
+//       if (!visited.has(neighbor)) {
+//         allVisited = false;
+//       }
+//     });
+
+//     if (allVisited) regions++;
+//   }
+//   return regions;
+// }
+
+function numRegions(graph) {
+  let regions = 0;
+  let visited = new Set();
+  
+  for (let node in graph) {
+    if (isNewRegion(node, graph, visited)) regions++;
   }
-  return nums;
+
+  return regions;
+}
+
+function isNewRegion(node, graph, visited) {
+  if (visited.has(node)) return false;
+  visited.add(node);
+
+  graph[node].forEach(neighbor => {
+    isNewRegion(neighbor, graph, visited);
+  });
+
+  return true;
 }
 
 module.exports = {
