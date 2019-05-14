dbe83e991926402a1304b5dc42a5ab6c2ddbaee5
your name here <gradyzhoo@gmail.com>
2019-05-13 22:08:35 -0700

    Ammend Heaps

--- heaps/lib/is_heap.js
+++ heaps/lib/is_heap.js
@@ -1,5 +1,29 @@
 // you may assume that the array will always have a null element at the 0-th index
+
+// super slow solution
+
+// function isMaxHeap(array, idx=1) {
+//   let current = array[idx];
+//   let leftChild = array[idx*2];
+//   let rightChild = array[idx*2+1];
+
+//   if (leftChild === undefined) leftChild = -Infinity;
+//   if (rightChild === undefined) rightChild = -Infinity;
+
+//   if (current < leftChild || current < rightChild) {
+//     return false;
+//   }
+
+//   while (idx < array.length) {
+//     idx++;
+//     if (isMaxHeap(array, idx) == false) return false;
+//   }
+
+//   return true;
+// }
+
 function isMaxHeap(array, idx=1) {
+  if (array[idx] === undefined) return true;
   let current = array[idx];
   let leftChild = array[idx*2];
   let rightChild = array[idx*2+1];
@@ -7,18 +31,8 @@ function isMaxHeap(array, idx=1) {
   if (leftChild === undefined) leftChild = -Infinity;
   if (rightChild === undefined) rightChild = -Infinity;
 
-  if (current < leftChild || current < rightChild) {
-    return false;
-  }
-
-  while (idx < array.length) {
-    idx++;
-    if (isMaxHeap(array, idx) == false) return false;
-  }
-
-  return true;
+  return (current > leftChild && current > rightChild && isMaxHeap(array, idx * 2) && isMaxHeap(array, idx * 2 + 1));
 }
-
 
 module.exports = {
     isMaxHeap
